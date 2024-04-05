dd5cfa6d43706a37134d5e59d8916cc2a19fc7a9
Chenyixu <910359910@qq.com>
2024-04-05 12:14:30 +0800

    noScan <add>生成模型

--- Code/autosarMBD.m
+++ Code/autosarMBD.m
@@ -12,34 +12,82 @@ classdef autosarMBD < handle
 
         function gen_tx_normal_model(obj, msg_name, msg_info)
             tx_normal_model = new_system(['cantx_0x' msg_name]);
-            open_system(tx_normal_model);             
+            open_system(tx_normal_model);
             for i_row = 1:height(msg_info)
+                
+                sig_model_path = ['cantx_0x' msg_name '/sig_' msg_info.Signal{i_row}];
+
                 if contains(msg_info.("Invalid Status"){i_row}, 'always FALSE')
-                    sig_model_path = ['cantx_0x' msg_name '/sig_' msg_info.Signal{i_row}];
                     add_block(obj.cantx_noerror, sig_model_path, 'Position', [1325, 270 + i_row * 90,1980, 330 + i_row * 90]);
 
-                    inport_handle = get_param([sig_model_path '/cantx_phy'], 'LineHandles').Outport;
-                    inport_name = ['cantx_diag_' msg_info.Signal{i_row} '_bsw'];
-                    set_param([sig_model_path '/cantx_phy'], 'Name', inport_name);
-                    set_param(inport_handle, 'Name', inport_name);
-
-                    mask_values = {msg_info.Offset{i_row}, msg_info.Factor{i_row}};
-                    set_param([sig_model_path '/Phy2Raw'], 'MaskValues', mask_values);
-
-                    set_param([sig_model_path '/Max'], 'Value', msg_info.Max{i_row});
-                    set_param([sig_model_path '/Min'], 'Value', msg_info.Min{i_row});
-
-                    outport_handle = get_param([sig_model_path '/cantx_raw'], 'LineHandles').Inport;
-                    outport_name = ['cantx_diag_' msg_info.Signal{i_row} '_raw'];
-                    set_param([sig_model_path '/cantx_raw'], 'Name', outport_name);
-                    set_param(outport_handle, 'Name', outport_name);
-
                 else
-                    sig_model_path = ['cantx_0x' msg_name '/sig_' msg_info.Signal{i_row}];
                     add_block(obj.cantx_error, sig_model_path, 'Position', [1325, 270 + i_row * 90,1980, 330 + i_row * 90])
 
+                    error_name = ['cantx_err_diag_' msg_info.Signal{i_row} '_bsw'];
+                    obj.modify_port_name('In', sig_model_path, 'cantx_inv_status', error_name)
+
                 end
+
+                inport_name = ['cantx_diag_' msg_info.Signal{i_row} '_bsw'];
+                obj.modify_port_name('In', sig_model_path, 'cantx_phy', inport_name)
+
+                outport_name = ['cantx_diag_' msg_info.Signal{i_row} '_raw'];
+                obj.modify_port_name('Out', sig_model_path, 'cantx_raw', outport_name)
+
+                mask_values = {msg_info.Offset{i_row}, msg_info.Factor{i_row}};
+                set_param([sig_model_path '/Phy2Raw'], 'MaskValues', mask_values);
+
+                set_param([sig_model_path '/Max'], 'Value', msg_info.Max{i_row});
+                set_param([sig_model_path '/Min'], 'Value', msg_info.Min{i_row});
+
+                obj.add_subsystem_port(sig_model_path)
             end
+
+            save_system(tx_normal_model);
+
+        end
+
+        function modify_port_name(obj, port_dire, mdl_path, ori_name, dst_name)
+
+            if strcmp(port_dire, 'In')
+                line_handle = get_param([mdl_path '/' ori_name], 'LineHandles').Outport;
+            elseif strcmp(port_dire, 'Out')
+                line_handle = get_param([mdl_path '/' ori_name], 'LineHandles').Inport;
+            end
+
+            set_param([mdl_path '/' ori_name], 'Name', dst_name);
+            set_param(line_handle, 'Name', dst_name);
+
+        end
+
+        function add_subsystem_port(obj, subsystem_path)
+            parent_path = get_param(subsystem_path, 'Parent');
+            subsystem_name = get_param(subsystem_path, 'Name');
+
+            inport_names = get_param(find_system(subsystem_path, 'SearchDepth', 1, 'BlockType', 'Inport'), 'Name');
+            in_handles = get_param(gcbh, 'PortHandles').Inport;
+            for i_in = 1:length(in_handles)
+
+                in_pos = get_param(get_param(subsystem_path, 'PortHandles').Inport(i_in), 'Position');
+                inport_pos =  [in_pos(1)-230, in_pos(2)-7, in_pos(1)-200, in_pos(2)+7];
+                add_block('simulink/Sources/In1', [parent_path '/' inport_names{i_in}], 'Name',inport_names{i_in},'Position', inport_pos)
+                in_line_handle = add_line(parent_path, [inport_names{i_in} '/1'], [subsystem_name '/' num2str(i_in)]);
+                set_param(in_line_handle, 'Name', inport_names{i_in});
+            end
+
+
+            outport_names = get_param(find_system(subsystem_path, 'SearchDepth', 1, 'BlockType', 'Outport'), 'Name');
+            out_handles = get_param(gcbh, 'PortHandles').Outport;
+            for i_out = 1:length(out_handles)
+
+                out_pos = get_param(get_param(subsystem_path, 'PortHandles').Outport(i_out), 'Position');
+                outport_pos =  [out_pos(1)+200, out_pos(2)-7, out_pos(1)+230, out_pos(2)+7];
+                add_block('simulink/Sinks/Out1', [parent_path '/' outport_names{i_out}], 'Name',outport_names{i_out},'Position', outport_pos)
+                out_line_handle = add_line(parent_path, [subsystem_name '/' num2str(i_out)], [outport_names{i_out} '/1']);
+                set_param(out_line_handle, 'Name', outport_names{i_out});
+            end
+
+
 
         end
 
